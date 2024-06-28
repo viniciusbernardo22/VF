a12ff244ef7b358b3a570c678ce43f7de7b751a0
Vinicius <vini383@gmail.com>
2024-06-28 15:20:20 -0300

    feat: padronized ErrorMessages.cs to centralize errorMessages between test and domain layer

---
+++ src/VF.Catalog.Domain/ErrorMessages/ErrorMessages.cs
@@ -0,0 +1,17 @@
+﻿namespace VF.Catalog.Domain;
+
+public class ErrorMessages
+{
+    public string EmptyOrNullMessage(string property)
+        => string.Format($"{property} should not be empty or null");
+    
+    public string MinLengthMessage(string property, int length)
+        => string.Format($"{property} should have a minimum of {length} characters long");
+    
+    public string MaxLengthMessage(string property, int length)
+        => string.Format($"{property} should have a maximum of {length} characters long");
+
+    public string ShouldNotBeNullMessage(string property)
+        => string.Format($"{property} should not be null");
+    
+}
